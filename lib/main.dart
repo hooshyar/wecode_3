e0ad088769362deb0a3215d3b417fc5c4b60123b
Mustafa <mcho.khalid@gmail.com>
2022-06-18 15:12:10 +0300

    delete myHomePage widget and put in seperated src folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:wecode_3/src/my_app.dart';
+import 'package:wecode_3/src/home.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'myApp',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Home(text: 'Kurdistan'),
+    );
+  }
 }
