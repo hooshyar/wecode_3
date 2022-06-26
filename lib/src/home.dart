28b2871554a2eeb07a31bc9a106c1c457a21272a
Mustafa <mcho.khalid@gmail.com>
2022-06-26 22:57:19 +0300

    change listview.builder() to work with data modal

--- lib/src/home.dart
+++ lib/src/home.dart
@@ -1,15 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:wecode_3/models/data_modal.dart';
 import 'package:wecode_3/src/person_detail.dart';
 import '../models/mock_data.dart';
 
-class Home extends StatefulWidget {
+class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
   static const routeName = '/';
-  @override
-  State<Home> createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +16,11 @@ class _HomeState extends State<Home> {
       body: ListView.builder(
           itemCount: data.length,
           itemBuilder: (ctx, index) {
+            //create a list of PersonModal and return data as a map
+            List<PersonModal> persons = data.map((element) {
+              //using named constructor from data_modal file
+              return PersonModal.fromMap(element);
+            }).toList();
             return Card(
               margin: const EdgeInsets.only(
                 top: 3,
@@ -31,18 +32,15 @@ class _HomeState extends State<Home> {
                 child: ListTile(
                   leading: CircleAvatar(
                     radius: 25,
-                    child: Image.network(data[index]['avatar'].toString()),
+                    child: Image.network(persons[index].avatar),
                   ),
-                  title: Text(data[index]['id'].toString()),
-                  subtitle: Text(data[index]['gender'].toString()),
+                  title: Text(persons[index].id),
+                  subtitle: Text(persons[index].gender),
                   trailing: IconButton(
                     onPressed: () {
                       Navigator.of(context).pushNamed(
                         PersonDetail.routeName,
-                        arguments: {
-                          'id': data[index]['id'].toString(),
-                          'index': index,
-                        },
+                        arguments: persons[index],
                       );
                     },
                     icon: const Icon(Icons.navigate_next),
